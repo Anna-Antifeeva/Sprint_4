916a5aee9777abe2860778badba436673979b6e5
Анна Антифеева <kadnikova.annett@yandex.ru>
2026-09-22 15:00:23 +0700

    Добавила descktop.ini в .gitignore

---
+++ src/test/java/pages/DriverFactory.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver() {
+        String browser = System.getProperty("browser", "chrome");
+
+        if (browser.equals("firefox")) {
+            return new FirefoxDriver();
+        } else {
+            return new ChromeDriver();
+        }
+    }
+}
